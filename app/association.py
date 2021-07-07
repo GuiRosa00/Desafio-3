c5cfc2c63e9e424b8c596575abd5173f6a3d4d86
mario jorge rosa rosa <guilherme.rosa@poli.ufrj.br>
2021-07-07 09:40:14 -0300

    Montado esqueleto inicial do app

--- app/association.py
+++ app/association.py
@@ -1,6 +1,6 @@
 from app.extensions import db
 from app.medicos.model import Medico
 from app.pacientes.model import Paciente
-association_med_pac = db.Table('association_med_pac',db.Column(),
-db.Column('id', db.Integer, db.ForeignKey('medico.id')),
-db.Column('id', db.Integer, db.ForeignKey('paciente.id')))
+association_med_pac = db.Table('association_med_pac',db.Model.metadata,
+db.Column('medico', db.Integer, db.ForeignKey('medico.id')),
+db.Column('paciente', db.Integer, db.ForeignKey('paciente.id')))
